a54c5a09c1555c7aa721413ed4b60313e969da5b
YunussEmree <senyigityunusemre@gmail.com>
2024-09-21 15:32:00 +0300

    Initial Commit

---
+++ src/test/java/com/YunussEmree/buyme/BuymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YunussEmree.buyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
