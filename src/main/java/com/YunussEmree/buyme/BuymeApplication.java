bae9ea770d3e2f1ff7b94a62d1cdd16481441e45
YunussEmree <senyigityunusemre@gmail.com>
2024-09-21 15:31:29 +0300

    Initial Commit

---
+++ src/main/java/com/YunussEmree/buyme/BuymeApplication.java
@@ -0,0 +1,13 @@
+package com.YunussEmree.buyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuymeApplication.class, args);
+	}
+
+}
