6d2fa0e9d448f0f8afd1648c83091a4fe7e8decc
YunussEmree <senyigityunusemre@gmail.com>
2024-09-24 12:41:52 +0300

    ProductNotFoundException is deleted. I created a more general class.

--- src/main/java/com/YunussEmree/buyme/core/utilities/exceptions/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.YunussEmree.buyme.core.utilities.exceptions;
-
-public class ProductNotFoundException extends RuntimeException{
-
-    public ProductNotFoundException(String message){
-        super(message);
-    }
-}
