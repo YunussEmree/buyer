a27d5165aa3a280e90950b4ab598ccef96d6da94
YunussEmree <senyigityunusemre@gmail.com>
2024-09-24 12:36:31 +0300

    Added new exceptions about resource

---
+++ src/main/java/com/YunussEmree/buyme/core/utilities/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.YunussEmree.buyme.core.utilities.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
