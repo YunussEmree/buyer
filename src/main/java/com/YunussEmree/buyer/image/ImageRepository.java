b35f0b7b08a3d64c90817e2b45f49c8dac5129e7
YunussEmree <senyigityunusemre@gmail.com>
2024-09-30 16:50:24 +0300

    new method for Image finding with Product Id

--- src/main/java/com/YunussEmree/buyer/image/ImageRepository.java
+++ src/main/java/com/YunussEmree/buyer/image/ImageRepository.java
@@ -2,5 +2,8 @@ package com.YunussEmree.buyer.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
+    List<Image> findByProductId(Long productId);
 }
