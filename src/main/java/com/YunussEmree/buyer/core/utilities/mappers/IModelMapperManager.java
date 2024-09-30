6e26dd2a85bd2de37acc8512ceb0e0a90012ca72
YunussEmree <senyigityunusemre@gmail.com>
2024-09-30 16:49:54 +0300

    removed old ModelMapper files

--- src/main/java/com/YunussEmree/buyer/core/utilities/mappers/IModelMapperManager.java
+++
@@ -1,9 +0,0 @@
-package com.YunussEmree.buyer.core.utilities.mappers;
-
-import org.modelmapper.ModelMapper;
-
-public interface IModelMapperManager {
-    ModelMapper forResponse();
-
-    ModelMapper forRequest();
-}
