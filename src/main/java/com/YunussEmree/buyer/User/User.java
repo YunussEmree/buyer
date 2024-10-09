306ebb631e8c3fb1c027e2f2f9d95e704bcad86a
Yunus Emre <86290901+YunussEmree@users.noreply.github.com>
2024-10-09 17:01:09 +0300

    Created User Classes, Interfaces and enums

--- src/main/java/com/YunussEmree/buyer/User/User.java
+++ src/main/java/com/YunussEmree/buyer/User/User.java
@@ -1,6 +1,8 @@
 package com.yunussemree.buyer.user;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,5 +16,15 @@ import lombok.Setter;
 @AllArgsConstructor
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+
+
+
 
 }
---
+++ src/main/java/com/YunussEmree/buyer/User/UserController.java
@@ -0,0 +1,9 @@
+package com.yunussemree.buyer.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class UserController {
+}
