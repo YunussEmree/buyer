8ddaab27af405bcb99c785b3377caa84822e6824
Yunus Emre <86290901+YunussEmree@users.noreply.github.com>
2024-10-10 16:25:18 +0300

    added findByUserId method

--- src/main/java/com/YunussEmree/buyer/order/OrderRepository.java
+++ src/main/java/com/YunussEmree/buyer/order/OrderRepository.java
@@ -2,5 +2,8 @@ package com.yunussemree.buyer.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
 }
