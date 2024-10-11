409850800631c884565d581fc259513804c9cee8
Yunus Emre <86290901+YunussEmree@users.noreply.github.com>
2024-10-11 19:15:21 +0300

    Deleted unnecessary file

--- src/main/java/com/YunussEmree/buyer/User/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.yunussemree.buyer.user;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping
-public class UserController {
-}
