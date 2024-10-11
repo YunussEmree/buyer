346f58d04c231a3b962f1ff2a022d6f52333687b
Yunus Emre <86290901+YunussEmree@users.noreply.github.com>
2024-10-11 19:17:45 +0300

    fixed endpoint issue

--- src/main/java/com/YunussEmree/buyer/controllers/OrderController.java
+++ src/main/java/com/YunussEmree/buyer/controllers/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/orders")
+@RequestMapping("${api.prefix}/orders")
 @RestController
 public class OrderController {
 
