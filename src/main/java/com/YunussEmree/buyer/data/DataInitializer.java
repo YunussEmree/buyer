f1184930a23e319d516e159273b6ecbd98520d90
Yunus Emre <86290901+YunussEmree@users.noreply.github.com>
2024-10-12 21:35:19 +0300

    Automated cart will be auto created when user no has cart

---
+++ src/main/java/com/YunussEmree/buyer/data/DataInitializer.java
@@ -0,0 +1,36 @@
+package com.yunussemree.buyer.data;
+
+import com.yunussemree.buyer.user.User;
+import com.yunussemree.buyer.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
+    private final UserRepository userRepository;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        createDefaultUserIfNotExits();
+    }
+
+
+    private void createDefaultUserIfNotExits(){
+        for (int i = 1; i<=5; i++){
+            String defaultEmail = "user"+i+"@email.com";
+            if (userRepository.existsByEmail(defaultEmail)){
+                continue;
+            }
+            User user = new User();
+            user.setFirstName("The User");
+            user.setLastName("User" + i);
+            user.setEmail(defaultEmail);
+            user.setPassword("123456");
+            userRepository.save(user);
+            System.getLogger("Default vet user " + i + " created successfully.");
+        }
+    }
+}
